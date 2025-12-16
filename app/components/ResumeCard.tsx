9a499732c38a7912c2bb9338aa16c492065f56da
Shreeiya Trivedi <shreeiyatrivedi@Shreeiyas-MacBook-Air.local>
2025-12-17 02:34:11 +0530

    Implemented wipe functionality

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -20,13 +20,13 @@ const ResumeCard = ({resume:{id, companyName, jobTitle, feedback, imagePath}} : 
         
         <div className='resume-card-header'>
             <div className='flex flex-col gap-2'>
-                {companyName &&<h2 className='!text-black font-bold break-words'>
+                {companyName &&<h2 className='text-black! font-bold wrap-break-word'>
                     {companyName}
                 </h2>}
-                {jobTitle && <h3 className='text-lg break-words text-gray-500'>
+                {jobTitle && <h3 className='text-lg wrap-break-word text-gray-500'>
                     {jobTitle}
                 </h3>}
-                {!companyName && !jobTitle && <h2 className='!text-black font-bold'>Resume</h2>}
+                {!companyName && !jobTitle && <h2 className='text-black! font-bold'>Resume</h2>}
             </div>
             <div className='flex-shrink-0'>
                 <ScoreCircle score={feedback.overallScore}/>
